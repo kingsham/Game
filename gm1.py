2b9291d0f3e2efe7eee9f02a3fc22eaaeef93ae1
Shamar Knibbs <shamar.knibbs@stu.bmcc.cuny.edu>
2018-05-21 11:18:13 -0400

    Update gm1.py

--- gm1.py
+++ gm1.py
@@ -2,25 +2,25 @@
 
 import random
 
-number = random.randint(1,10)
+number = random.randint(1,10) # Generates numbers 1 through 10 randomly
 win=False
 tries=0
-username=input("What is you username ?")
+username=input("What is you username ?") # user inputs their name
 
-print("Hello",username + ",",)
+print("Hello",username + ",",) 
 
-question=input("Would you like to play a game?[Y/N]")
+question=input("Would you like to play a game?[Y/N]") # The program asks the user if he/she wants to play or not
 if question.lower() == "n":
     print("oh..okay")
     exit()
 if question.lower() == "y":
-    print("I'm thinking of a number between 1 & 10")
+    print("I'm thinking of a number between 1 & 10") 
 while not win:
-    guess = int(input("Have a guess: "))
+    guess = int(input("Have a guess: ")) # Allows user to enter the guess number
     tries=tries+1
-    if guess == number:
+    if guess == number: # if the the number the user entered is correct you have won
         win = True
-    elif guess < number:
+    elif guess < number: # if the number the user entered is lower than the guess number then the program will say the print statement
         print("Guess Higher")
     elif guess > number:
         print("Guess Lower")
